759f7a7f2c0ee4d724c7a345d444eee8c92116b4
Łukasz Węgiel <lukasz@mirumee.com>
2016-06-23 12:56:58 +0200

    Add new stateless components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import SearchBar from '../components/SearchBar'
+import GifList from '../components/GifList';
+import SearchBar from '../components/SearchBar';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  }
+
   handleTermChange(term) {
     console.log(term);
   }
@@ -11,6 +33,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar onTermChange={this.handleTermChange}/>
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
